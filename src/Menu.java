c5068204384707adfe8fff7b5ee4cce36b4a56d4
Lucas Maques <42378708+WottyMark@users.noreply.github.com>
2026-09-18 15:28:13 -0300

    Geracao de menus

--- src/Menu.java
+++ src/Menu.java
@@ -3,7 +3,7 @@ public class Menu
     private String adorno = "================================================================================\n";
     private int nOfSpaces;
     
-    public void GenerateMenuTitle(String title)
+    public void GenerateMenuTitle(String title, Boolean useHeader, String header)
     {
         System.out.println(adorno);
         nOfSpaces = Math.floor((80 - title.length())/2)
@@ -11,7 +11,27 @@ public class Menu
         {
             System.out.print(" ");
         }
-        System.out.print(title + "\n\n");
-        System.out.print(adorno);
+        System.out.print(title + "\n");
+        if useHeader
+        {
+            nOfSpaces = Math.floor((80 - header.length())/2)
+            for(int i = 0;i<nOfSpaces;i++)
+            {
+                System.out.print(" ");
+            }
+            System.out.print(header + "\n");
+        }
+        System.out.print("\n");
+        System.out.println(adorno);
+        System.out.print("\n");
+    }
+    
+    public void GenerateMenu(String subheader, int start, ArrayList<String> options)
+    {
+        System.out.println("  " + subheader + "\n");
+        for(int i = start;i <= (options.size() + start - 1);i++)
+        {
+            System.out.println(i + " - " + options.get(i - start));
+        }
     }
 }
