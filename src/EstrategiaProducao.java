dc1b9f83257ce18a645ec401f597580b1ef69202
Lucas Maques <42378708+WottyMark@users.noreply.github.com>
2026-09-21 11:45:44 -0300

    Teste 2

--- src/EstrategiaProducao.java
+++ src/EstrategiaProducao.java
@@ -2,8 +2,32 @@ import java.util.ArrayList;
 
 public interface EstrategiaProducao
 {
-    public Demanda selecionarDemanda(ArrayList<Demanda> demandas);
+    public Demanda selecionarDemanda(ArrayList<Demanda> demandas, double orcamentoDisponivel);
     public String getNomeEstrategia();
 }
 
+public class EstrategiaFiFo implements EstrategiaProducao
+{
+    public Demanda selecionarDemanda(ArrayList<Demanda> demandas, double orcamentoDisponivel)
+    {
 
+    }
+
+    public String getNomeEstrategia()
+    {
+
+    }
+}
+
+public class EstrategiaMaiorDemanda implements EstrategiaProducao
+{
+    public Demanda selecionarDemanda(ArrayList<Demanda> demandas, double orcamentoDisponivel)
+    {
+
+    }
+
+    public String getNomeEstrategia()
+    {
+
+    }
+}
